74dd337eb6d3f057f104ade146288dfab6379fac
AkiNeko11 <8300895@qq.com>
2025-09-12 20:10:14 +0800

    完成服务器静态文件部署

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path'); 
 const authRoutes = require('./routes/auth.routes');
 const roomRoutes = require('./routes/room.routes');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../../frontend'))); 
 app.use('/api/auth', authRoutes);
-app.use('/api/rooms', roomRoutes); 
+app.use('/api/rooms', roomRoutes);
+
+// 根路径路由 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../frontend/html/index.html'));
+});
 
 module.exports = {
     app
